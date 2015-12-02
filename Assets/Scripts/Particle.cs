a3a7f5ae4cfe7f9dea5ddd6b1ff4a58f51c2fddb
Chui <lordbabypanda@gmail.com>
2015-12-02 09:51:11 -0600

    Physics
    Folders included.

---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Unity has build in normailized and Mag. */
+
+public class Particle : MonoBehaviour 
+{
+	public Vector3 position;
+	public Vector3 velocity;
+	public Vector3 force;
+	public Vector3 acceleration;
+	public float mass;
+	
+	// r = pos
+	// v = velocity
+	// a = acceleration
+	// m = mass
+	// p = momentum
+	// f = force
+	// l = length;
+	
+	// Didn't do Uniform Gravity yet, using rigidbody, but thinking about it.
+	
+	public void Acceleration()
+	{
+		acceleration = (1 / mass) * force;
+	}
+	
+	public void Force ()
+	{
+		// F = ma;
+		force = mass * acceleration;
+	}
+	
+	// Euler Integration for Velocity
+	public void Velocity()
+	{
+		velocity = velocity + (acceleration * Time.deltaTime);
+	}
+	
+	// Euler Integration for Position
+	public void Position()
+	{
+		position = position + (velocity * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/SpringDamper.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpringDamper : MonoBehaviour 
+{
+	public float SpringConstant, DampingFactor, RestLength;
+	Particle p1, p2;
+	
+	// r = pos
+	// v = velocity
+	// a = acceleration
+	// m = mass
+	// p = momentum
+	// f = force
+	
+	// force spring = k_s = spring constant
+	// force damp = k_d = damping factor
+	// Spring Damper = l_o = rest length
+	
+	// Force Spring = Spring Constant( Length - Length)
+	// Force Damp = Damping Factor = Damping Factor(
+	
+	public void ComputeForce()
+	{
+		float forceSpring = SpringConstant * (RestLength - RestLength);
+		//float dampingForce = DampingFactor * (p1.velocity - p2.velocity);
+		//float springDamper = 
+	}
+}
