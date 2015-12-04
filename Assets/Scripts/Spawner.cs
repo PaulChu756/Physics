aef0b5728be9efa78ebde40b42445a85b091b32c
Chui <lordbabypanda@gmail.com>
2015-12-04 17:34:43 -0600

    Physics
    Trying to do springs still.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,47 +4,110 @@ using System.Collections.Generic;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject particle;
-    static GameObject node;
-    //public GameObject SpringDamper;
+    public GameObject SpringDamper;
+    public GameObject P1;
+    public GameObject P2;
 
     public List<GameObject> particleList = new List<GameObject>();
     public List<GameObject> springdamp = new List<GameObject>();
+
+    public float k;
+    public float d;
+    public float b;
+    public float stiff;
 
     public int rows = 5, cols = 5, width, length;
 
     void Awake()
     {
-        node = particle;
-        Spawn();
+        SpawnParticles();
+        //SpawnSpringDampers();
     }
 
-    void Update()
-    {
-        ApplyGravity();
-    }
-
-    public void Spawn()
+    public void SpawnParticles()
     {
         for (int i = 0; i < rows; i++)
         {
             for (int j = 0; j < cols; j++)
             {
-                GameObject o;
-                o = Instantiate(node) as GameObject;
+                GameObject o = Instantiate(P1);
+                o.transform.parent = gameObject.transform;
                 o.transform.position = new Vector3(i * width / rows, j * length / cols, 0);
                 particleList.Add(o);
             }
         }
-
     }
+
+
 
     public void ApplyGravity()
     {
         foreach(GameObject o in particleList)
         {
-            Vector3 g = new Vector3(0, -9.8f, 0);
-            GetComponent<Particle>().force = GetComponent<Particle>().mass * g;
+            Vector3 g = new Vector3(0, -9.8f, 0) * GetComponent<Particle>().mass;
+            GetComponent<Particle>().force += g;
         }
     }
 }
+
+
+//public void SpawnSpringDampers()
+//{
+//    for(int i = 0; i < particleList.Count; i++)
+//    {
+//        if(i > width - 1)
+//        {
+//            if(particleList[i - width]) // Top
+//            {
+//                GameObject o = new GameObject("Spring");
+//                o.transform.parent = gameObject.transform;
+//                o.AddComponent<SpringDamper>();
+//                MakeSpring(particleList[i], particleList[i - width], stiff, 175);
+//                springdamp.Add(o);
+//            }
+//        }
+
+//        if((i % width != width - 1) && i >= width) // Right
+//        {
+//            GameObject o = new GameObject("Spring");
+//            o.transform.parent = gameObject.transform;
+//            o.AddComponent<SpringDamper>();
+//            MakeSpring(particleList[i], particleList[i - width], stiff,175);
+//            springdamp.Add(o);
+//        }
+
+//        if (i > 0) // Left 
+//        {
+//            if (i % width != 0)
+//            {
+//                GameObject o = new GameObject("Spring");
+//                o.transform.parent = gameObject.transform;
+//                o.AddComponent<SpringDamper>();
+//                MakeSpring(particleList[i], particleList[i - 1], stiff, 175);
+//                springdamp.Add(o);
+//            }
+//        }
+//    }
+//}
+
+//public float Distance(Vector3 a, Vector3 b)
+//{
+//    float x = b.x - a.x;
+//    float y = b.y - a.x;
+//    float z = b.z - a.z;
+
+//    return Mathf.Sqrt(x * x + y * y + z * z);
+//}
+//public void MakeSpring(GameObject p1, GameObject p2, float stiff, float damper)
+//{
+//    P1 = p1;
+//    P2 = p2;
+//    k = stiff;
+//    d = Distance(P1.transform.position, P2.transform.position);
+//    b = damper;
+//}
+
+//public void Draw()
+//{
+//    Debug.DrawLine(P1.transform.position, P2.transform.position);
+//}
--- Assets/Scripts/SpringDamper.cs
+++ Assets/Scripts/SpringDamper.cs
@@ -9,14 +9,13 @@ public class SpringDamper : MonoBehaviour
     // Spring constant = Ks
     // Damping Factor = Kd
     // Rest Length = l
-
     // spring force = -k * d || k = force but number is scaler. d = displacement, number is vector
 
     public void ComputeForce()
     {
         // Spring Force
         // Get the distance between two particles
-        // So first we p2 Pos - p1 pos
+        // So first we use  p2 Pos - p1 pos
         // Then we normalized e
         // Then use Vector.Distance to find out the distance between particles, in order to get the length
         // After getting the length, use in equation to get Spring Force.
@@ -25,8 +24,11 @@ public class SpringDamper : MonoBehaviour
         float distanceBetween = Vector3.Distance(p1.position, p2.position);
         float forceSpring = -springConstant * (RestLength - distanceBetween);
 
-        // Linear Damping Force = -dampingFactor (v1 - v2)
-        // Turn 1D Velocities
+        // Linear Damping Force
+        // Kd = -dampingFactor (v1 - v2)
+        // Turn 3D distances & velocities to 1D
+        // Compute spring force in 1D
+        // turn 1D force back into 3D force
         float v1 = Vector3.Dot(l, p1.velocity);
         float v2 = Vector3.Dot(l, p2.velocity);
         float dampingForce = -dampingFactor * (v1 - v2);
@@ -40,21 +42,21 @@ public class SpringDamper : MonoBehaviour
         p1.force += force1;
         p2.force += force2;
 
-            // Aerodynamic drag force
-     Vector3 F_aero;
-     Vector3 p;
-     Vector3 c;
-     Vector3 a;
-     //Vector3 e;
+        //Vector3 F_aero;
+        //Vector3 p;
+        //Vector3 c;
+        //Vector3 a;
+        //Vector3 e;
     }
 }
 
-        // p = density of air or water
-        // c = coefficient of drag for the object
-        // a = cross sectional area of the object
-        // e = unity vector in the opposite direction of the velocity // Changed to n for some reason.
-        // What the hell is V... Velocity?!?!?
+// Aerodynamic drag force
+// p = density of air or water
+// c = coefficient of drag for the object
+// a = cross sectional area of the object
+// e = unity vector in the opposite direction of the velocity // Changed to n for some reason.
+// What the hell is V... Velocity?!?!?
 
-        // Using triangles for surface area
-        // find velocity, normal, and area of triangle.
+// Using triangles for surface area
+// find velocity, normal, and area of triangle.
 
