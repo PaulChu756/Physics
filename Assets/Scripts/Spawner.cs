2945c9ab3856947a73d4549972f5fc91492e0841
Chui <lordbabypanda@gmail.com>
2015-12-07 15:24:08 -0600

    Physics
    Spawn Springs and particles correctly.

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -8,7 +8,8 @@ public class Spawner : MonoBehaviour
     public List<Spring> springs = new List<Spring>();
 
     public Particle particle;
-    public Spring spring;
+    public Spring sideSpring;
+    public Spring upSpring;
 
     public int width, height;
 
@@ -28,18 +29,34 @@ public class Spawner : MonoBehaviour
                 newParticle = particle;
                 particles.Add(newParticle);
 
-                if (x > 0) // Make sure this isn't the first particle in the row, if there is, there won't be anything to it's left and we can't connect. Left side
+                if (x > 0) // Left spring
                 {
+                    // Make sure this isn't the first particle in the row, if there is, there won't be anything to it's left and we can't connect.
                     //connects to the part we just created with the particle at the index.
                     // particle at the same Y value.
                     // x - 1 we're creating all the particles one by one, but storing.
-                    //Spring leftSpring = (newParticle, particles[y * width + (x - 1)]);
-                    //leftSpring = Instantiate(spring);
-                    //springs.Add(leftSpring);
-                    Spring leftSpring = Instantiate(spring);
+                    Spring leftSpring = Instantiate(sideSpring);
                     leftSpring.makeSpring(newParticle, particles[y * width + (x - 1)]);
-                    leftSpring.transform.position = new Vector3(x * width, y * height, 0);
+                    leftSpring.transform.position = new Vector3((x - .5f) * width, y * height, 0);
                     springs.Add(leftSpring);
+                }
+
+                if (y > 0)
+                {
+                    Spring aboveSpring = Instantiate(upSpring);
+                    aboveSpring.makeSpring(newParticle, particles[(y - 1) * width + x]);
+                    aboveSpring.transform.position = new Vector3(x * width, (y - .5f) * height, 0);
+                    springs.Add(aboveSpring);
+                }
+
+                foreach(Particle p in particles)
+                {
+                    p.GetComponent<Spring>().Simulate();
+                }
+                
+                foreach(Spring s in springs)
+                {
+                    s.GetComponent<Spring>().Simulate();
                 }
             }
         }
